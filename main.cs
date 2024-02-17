2ef6223e5bb6dafe722a780d496c77c47302627b
jaynzle <158656853+jaynzle@users.noreply.github.com>
2024-02-17 02:22:43 +0000

    1

--- main.cs
+++ main.cs
@@ -4,6 +4,6 @@ class Program {
   public static void Main (string[] args) {
     Console.WriteLine("Enter something: ");
     string input = Console.ReadLine();
-    Console.WriteLine(input)
+    Console.WriteLine(input);
   }
 }
