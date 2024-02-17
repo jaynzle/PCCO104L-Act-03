53cb15d0d23e582f2732d533f198e0e2918cecda
jaynzle <158656853+jaynzle@users.noreply.github.com>
2024-02-17 02:21:14 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine("Enter something: ");
+    string input = Console.ReadLine();
+    Console.WriteLine(input)
+  }
+}
