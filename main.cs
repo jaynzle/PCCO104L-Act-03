319690ab3506079e9162b1434acc36218b019ac4
nathan <158656853+jaynzle@users.noreply.github.com>
2024-03-11 07:45:03 +0000

    3

--- main.cs
+++ main.cs
@@ -3,15 +3,17 @@ using System;
 class Program {
   public static void Main (string[] args) {
     string input;
+    string previousInput = "";
 
     do
     {
       Console.WriteLine("Enter something: ");
       input = Console.ReadLine();
-      Console.WriteLine(input);
+      Console.WriteLine(previousInput + " " + input);
+      previousInput += " " + input;
     }
     while (input != "exit");
 
     Console.WriteLine("Program closed.");
-    }
+  }
 }
